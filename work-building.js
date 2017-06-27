e5f12f956d9f6a89787863ccdb53a0f7bb938b8b
Max Donchenko <wereya2@yandex.ru>
2017-06-27 18:18:38 +0300

    Wrap actions in functions + add logger

--- work-building.js
+++ work-building.js
@@ -1,17 +1,36 @@
 var outerFrameDoc = document.querySelector('frame[src="/main_frame.php"]').contentWindow.document;
 var innerFrameDoc = outerFrameDoc.querySelector('iframe[src="building/index.php"]').contentWindow.document;
 
-var formElem = innerFrameDoc.querySelector('form[action="/building/index.php"]');
-console.log('Elem: <form>');
-var selectElement = formElem.querySelector('select[name="kind"]');
-if (selectElement) {
-    [].slice
-      .call(selectElement.children)
-      .reverse()
-      .find(e => e.style.color === 'green')
-      .setAttribute('selected', true)
+hideBuildingImg();
+startWork();
 
-    console.log('Elem: <select>');
+function hideBuildingImg() {
+    var imgElem = innerFrameDoc.querySelector('img[src*="/img/locations/nd/jpg/satellite_street/summer/stroyka"]');
+    imgElem.hidden = true;
+};
+
+function startWork() {
+    var formElem = innerFrameDoc.querySelector('form[action="/building/index.php"]');
+    logger('Elem: <form>');
+    var selectElement = formElem.querySelector('select[name="kind"]');
+    if (selectElement) {
+        [].slice
+          .call(selectElement.children)
+          .reverse()
+          .find(e => e.style.color === 'darkgreen')
+          .setAttribute('selected', true)
+
+        logger('Elem: <select>');
+    }
+    var beginWorkBtn = formElem.querySelector('input[value="РќР°С‡Р°С‚СЊ СЂР°Р±РѕС‚Р°С‚СЊ"]');
+    beginWorkBtn && beginWorkBtn.click && beginWorkBtn.click() && logger('Elem: <input>');
 }
-var beginWorkBtn = formElem.querySelector('input[value="Начать работать"]');
-beginWorkBtn && beginWorkBtn.click && beginWorkBtn.click() && console.log('Elem: <input>');
+
+function refreshPage() {
+    var refreshBtn = innerFrameDoc.querySelector('div[title="РћР±РЅРѕРІРёС‚СЊ"]');
+    refreshBtn && refreshBtn.click && refreshBtn.click();
+}
+
+function logger(data) {
+    console.log(`${new Date().toLocaleString()} |`, data)
+}
